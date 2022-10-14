c72ca54493b0b2b13b3a5818fce3f5043050320a
shashanksingh1717 <107427465+shashanksingh1717@users.noreply.github.com>
2022-10-15 04:02:12 +0530

    Update helper.py

--- ABG/helper.py
+++ ABG/helper.py
@@ -1,6 +1,6 @@
-HELP_IMG = ["https://telegra.ph/file/2ad7c9d508b26c3cc7c09.jpg"]
+HELP_IMG = ["https://telegra.ph//file/3270d18502ddabfebe9a0.jpg"]
 
-START_IMG = ["https://telegra.ph/file/2ad7c9d508b26c3cc7c09.jpg"]
+START_IMG = ["https://telegra.ph//file/84903e9f61b2301aeeda3.jpg"]
 
 
-PHOTO = ["https://telegra.ph/file/2ad7c9d508b26c3cc7c09.jpg"]
+PHOTO = ["https://telegra.ph//file/60817d16e999a7540d35f.jpg"]
